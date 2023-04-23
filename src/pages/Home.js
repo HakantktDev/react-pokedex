b2236adec2b807ebf5f4a40cbf4f4c0556be5856
Hakan Tokat <hakan.tokat@interprobe.com.tr>
2023-04-23 14:54:49 +0300

    bug-fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -61,7 +61,7 @@ const HomePage = () => {
   }
   return (
     <>
-      <div className="h-800">
+      <div className="minh-800">
         <PokemonList pokemons={pokemons} data-testid="loader" />
       </div>
       <Pagination gotoNextPage={nextPageUrl ? gotoNextPage : null} gotoPrevPage={prevPageUrl ? gotoPrevPage : null} />
